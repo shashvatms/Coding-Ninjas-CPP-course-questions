4fb6b1dd9f80f97c1ba8575aaaee1b8cd78cdeb1
shashvatms <119059021+shashvatms@users.noreply.github.com>
2024-01-09 23:17:03 +0530

    Create  Leaders in array.cpp

---
+++ test 2/Leaders in array.cpp
@@ -0,0 +1,54 @@
+v8.0  Premium Features
+HARPA AI v8 introduces first PRO features:
+⚙️ Customize preconfigured commands.
+📚 Reuse chats across browser tabs, access and manage AI chat history.
+🌤️ Access your commands anywhere with cloud storage linked to your HARPA AI profile.
+⚡️ Experience HARPA's full capabilities without any limits: get lifetime access with the HARPA X plan or choose the flexible monthly S plan.
+
+
+
+
+
+RESEARCH
+/search-agent
+
+EXPLAIN
+/explain
+
+CHAT WITH PAGE
+/ask
+
+SHORT SUMMARY
+/summary
+
+LONG SUMMARY
+/summary
+
+EXTRACT DATA
+/extract-data
+
+REPURPOSE PAGE
+/repurpose-text
+
+MONITOR PAGE
+
+REPLY TO TEXT
+/reply
+
+ALL COMMANDS
+
+
+CONNECT TO GPT
+To use AI features, please login to chat.openai.com
+
+LOGIN
+
+Not connecting?
+Try to switch to Tab Mode, clear openai.com cookies, turn off VPN, or enter your OpenAI API Key
+
+
+Use Tab Mode
+
+
+
+
